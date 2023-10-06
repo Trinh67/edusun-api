3d83e6d1bea0ffc2f061a21601ef6798ef2c7454
trinh.tx <trinh.tx@teko.vn>
2023-10-06 11:15:16 +0700

    add define api job post

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1 import health, config, common, user
+from app.api.v1 import health, config, common, user, job_post
 
 v1_router = APIRouter()
 
@@ -8,4 +8,5 @@ v1_router.include_router(health.router, prefix="/health", tags=['health-check'])
 v1_router.include_router(common.router, prefix="/common", tags=['common'])
 v1_router.include_router(user.router, prefix="/users", tags=['users'])
 v1_router.include_router(config.router, prefix="/configs", tags=['configs'])
+v1_router.include_router(job_post.router, prefix="/job-posts", tags=['job-posts'])
 
